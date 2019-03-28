2c59692288d55687764688ea6c5ff750ee24e341
tenghaoxiang <873030913@qq.com>
2019-03-28 09:51:04 +0800

    2019.3.28

--- src/sort/Main.java
+++ src/sort/Main.java
@@ -22,7 +22,7 @@ public class Main {
             }
         }
         input.close();
-        ShellSort.shellSort(array);
+        MergeSortUpDown.mergeSortUpDown(array);
         for (T t : array) {
             System.out.print(t + " ");
         }
---
+++ src/sort/MergeSortUpDown.java
@@ -0,0 +1,40 @@
+package sort;
+
+public class MergeSortUpDown {
+
+    private static Comparable[] copy;
+
+    public static <T extends Comparable<? super T>> void mergeSortUpDown(T[] a) {
+        copy = new Comparable[a.length];
+        mergeSortUpDown(a, 0, a.length - 1);
+    }
+
+    public static <T extends Comparable<? super T>> void mergeSortUpDown(T[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int mid = lo + (hi-lo)/2;
+        mergeSortUpDown(a, lo, mid);
+        mergeSortUpDown(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    public static <T extends Comparable<? super T>> void merge(T[] a, int lo, int mid, int hi) {
+        int m = lo, n = mid + 1;
+        for (int i = 0; i < a.length; i++) {
+            copy[i] = a[i];
+        }
+        for (int i = lo; i <= hi; i++) {
+            if (m > mid) {
+                a[i] = (T) copy[n++];
+            } else if (n > hi) {
+                a[i] = (T) copy[m++];
+            } else if (copy[m].compareTo(copy[n]) < 0) {
+                a[i] = (T) copy[m++];
+            } else {
+                a[i] = (T) copy[n++];
+            }
+        }
+    }
+
+}
