be55191df16c1fe48e5f4551398eee5cfb9ef59f
tenghaoxiang <873030913@qq.com>
2019-04-06 09:32:40 +0800

    2019.4.6 判断回文，记得先判断字符串是否为空

---
+++ src/leetcode/Solution125.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+/**
+ * 判断回文，只考虑数字和字母，不考虑符号
+ * 要先判断字符是否为空
+ */
+public class Solution125 {
+
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while(head <= tail) {
+            cHead = s.charAt(head);
+            cTail = s.charAt(tail);
+            if (!Character.isLetterOrDigit(cHead)) {
+                head++;
+            } else if(!Character.isLetterOrDigit(cTail)) {
+                tail--;
+            } else {
+                if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+        }
+        return true;
+    }
+
+}
