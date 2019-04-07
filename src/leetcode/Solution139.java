8ece6b484494d1907c4deddbb7223c6aa401d1bb
tenghaoxiang <873030913@qq.com>
2019-04-07 22:36:21 +0800

    2019.4.7 ※dp经典

--- src/leetcode/Solution139.java
+++ src/leetcode/Solution139.java
@@ -6,23 +6,38 @@ public class Solution139 {
 
     public boolean wordBreak(String s, List<String> wordDict) {
 
-        String[] array = wordDict.toArray(new String[0]);
-        int head = 0;
-        while (head < s.length()) {
-            int k = head;
-            for (int i = 0; i < array.length; i++) {
-                int length = array[i].length();
-                if (head + length <= s.length()) {
-                    if (s.substring(head, head + length).equals(array[i])) {
-                        head = head + length;
-                    }
+        boolean[] f = new boolean[s.length() + 1];
+        f[0] = true;
+
+        /**
+         * 动态规划经典思路，一定要理解
+         */
+//        for(int i = 1; i <= s.length(); i++){
+//            for(String str: wordDict){
+//                if(str.length() <= i){
+//                    if(f[i - str.length()]){
+//                        if(s.substring(i-str.length(), i).equals(str)){
+//                            f[i] = true;
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
+        /**
+         * 第二章dp方法
+         */
+        for(int i=1; i <= s.length(); i++){
+            for(int j=0; j < i; j++){
+                if(f[j] && wordDict.contains(s.substring(j, i))){
+                    f[i] = true;
+                    break;
                 }
             }
-            if (k == head) {
-                return false;
-            }
         }
-        return true;
+
+        return f[s.length()];
 
     }
 
