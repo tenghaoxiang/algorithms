522a01aad254de23de88e4a720985495e667ef15
tenghaoxiang <873030913@qq.com>
2019-04-08 12:54:33 +0800

    2019.4.8

---
+++ src/leetcode/Solution344.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class Solution344 {
+
+    public void reverseString(char[] s) {
+
+        char c;
+        for (int i = 0; i < s.length / 2; i++) {
+            c = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = c;
+        }
+
+    }
+
+}
