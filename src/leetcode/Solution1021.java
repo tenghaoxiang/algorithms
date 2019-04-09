ee964d763126071a35e5381d65a4d5aaef4375d1
tenghaoxiang <873030913@qq.com>
2019-04-09 16:55:02 +0800

    2019.4.9

---
+++ src/leetcode/Solution1021.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class Solution1021 {
+
+    /**
+     * 注意opened++和opened--执行的次序
+     */
+    public String removeOuterParentheses(String S) {
+        StringBuilder s = new StringBuilder();
+        int opened = 0;
+        for (char c : S.toCharArray()) {
+            if (c == '(' && opened++ > 0) s.append(c);
+            if (c == ')' && opened-- > 1) s.append(c);
+        }
+        return s.toString();
+    }
+
+}
