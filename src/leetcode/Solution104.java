f49dc8c208ba5fb256c8c86957823212239366d0
tenghaoxiang <873030913@qq.com>
2019-04-10 09:27:16 +0800

    2019.4.10 ※ 运用递归，求解树的最大深度

---
+++ src/leetcode/Solution104.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class Solution104 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+
+    }
+
+}
---
+++ src/leetcode/Solution146.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class Solution146 {
+
+
+
+}
+
+class LRUCache {
+    public LRUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+
+        return 0;
+
+    }
+
+    public void put(int key, int value) {
+
+
+
+    }
+}
