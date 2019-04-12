6b0f74e8b435cc2176948117f40dfb36f78390c2
tenghaoxiang <873030913@qq.com>
2019-04-12 10:09:19 +0800

    2019.4.12 一个队列实现一个栈

---
+++ src/leetcode/Solution225.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution225 {
+}
+
+class MyStack {
+
+    Queue<Integer> queue;
+
+    public MyStack() {
+        this.queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue.add(x);
+        for (int i = 0; i < queue.size() - 1; i++) {
+            queue.add(queue.poll());
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
