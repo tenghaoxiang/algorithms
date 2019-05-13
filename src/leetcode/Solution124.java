3a1236ff0ea782df08b809e67a2ef39b40e18bc6
tenghaoxiang <873030913@qq.com>
2019-05-13 12:30:33 +0800

    2019.5.13 ※ 求二叉树中的最小路径和，经典

---
+++ src/leetcode/Solution124.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+public class Solution124 {
+
+    class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int x) {
+            this.val = x;
+        }
+
+    }
+
+    int maxValue;
+
+    public int maxPathSum(TreeNode root) {
+
+        maxValue = Integer.MIN_VALUE        ;
+        maxPathDown(root);
+        return maxValue;
+
+    }
+
+    private int maxPathDown(TreeNode node) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
+
+        maxValue = Math.max(maxValue, left + right + node.val);
+
+        return Math.max(left, right) + node.val;
+
+    }
+
+}
