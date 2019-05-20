091402eeacc77457f20168fb85feb52b6c1729ea
tenghaoxiang <873030913@qq.com>
2019-05-20 20:53:28 +0800

    2019.5.20 晚上学习，白天睡觉

--- src/offer/Solution25.java
+++ src/offer/Solution25.java
@@ -1,4 +1,38 @@
 package offer;
 
 public class Solution25 {
+
+    class ListNode{
+        int val;
+        ListNode next = null;
+
+        public ListNode(int x) {
+            this.val = x;
+        }
+    }
+
+    public ListNode Merge(ListNode list1,ListNode list2) {
+
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode head = null;
+
+        if (list1.val < list2.val) {
+            head = list1;
+            head.next = Merge(list1.next, list2);
+        } else {
+            head = list2;
+            head.next = Merge(list1, list2.next);
+        }
+
+        return head;
+
+    }
+
 }
