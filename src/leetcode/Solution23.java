605819fc101180829f4abd934ff6eca500314086
tenghaoxiang <873030913@qq.com>
2019-05-22 17:35:45 +0800

    2019.5.22 ※ 链表专题-合并K个有序链表

---
+++ src/leetcode/Solution23.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+public class Solution23 {
+
+    class ListNode {
+        int val;
+        ListNode next = null;
+
+        public ListNode(int x) {
+            this.val = x;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        if (lists.length == 0 && lists == null) {
+            return null;
+        }
+        return partition(lists, 0, lists.length - 1);
+
+    }
+
+    public ListNode partition(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        if (start < end) {
+            int mid = start + (end - start) / 2;
+            ListNode left = partition(lists, start, mid);
+            ListNode right = partition(lists, mid + 1, end);
+            return merge(left, right);
+        } else {
+            return null;
+        }
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+    }
+
+}
