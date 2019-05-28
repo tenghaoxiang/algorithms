2118501190c52085b2f1ea47473537ec381b8b43
tenghaoxiang <873030913@qq.com>
2019-05-28 23:38:20 +0800

    2019.5.28 ※ 树专题-将树的每一层存到一个数组中

--- src/leetcode/Solution102.java
+++ src/leetcode/Solution102.java
@@ -18,30 +18,51 @@ public class Solution102 {
     }
 
     public List<List<Integer>> levelOrder(TreeNode root) {
-
         List<List<Integer>> res = new ArrayList<>();
-        if (root == null) {
-            return res;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            List<Integer> level = new ArrayList<>();
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                level.add(node.val);
-                if (node.left != null) {
-                    queue.add(node.left);
-                }
-                if (node.right != null) {
-                    queue.add(node.right);
-                }
-            }
-            res.add(level);
-        }
+        levelHelper(res, root, 0);
         return res;
-
     }
+
+    public void levelHelper(List<List<Integer>> res, TreeNode root, int height) {
+        if (root == null) {
+            return;
+        }
+        if (height >= res.size()) {
+            res.add(new LinkedList<Integer>());
+        }
+        res.get(height).add(root.val);
+        levelHelper(res, root.left, height + 1);
+        levelHelper(res, root.right, height + 1);
+    }
+
+    /**
+     * 使用非递归，队列实现
+     */
+//    public List<List<Integer>> levelOrder(TreeNode root) {
+//
+//        List<List<Integer>> res = new ArrayList<>();
+//        if (root == null) {
+//            return res;
+//        }
+//        Queue<TreeNode> queue = new LinkedList<>();
+//        queue.add(root);
+//        while (!queue.isEmpty()) {
+//            List<Integer> level = new ArrayList<>();
+//            int size = queue.size();
+//            for (int i = 0; i < size; i++) {
+//                TreeNode node = queue.poll();
+//                level.add(node.val);
+//                if (node.left != null) {
+//                    queue.add(node.left);
+//                }
+//                if (node.right != null) {
+//                    queue.add(node.right);
+//                }
+//            }
+//            res.add(level);
+//        }
+//        return res;
+//
+//    }
 
 }
