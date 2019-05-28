1b678a0799aa9803ce6dc55dba269d7620347930
tenghaoxiang <873030913@qq.com>
2019-05-28 13:45:26 +0800

    2019.5.28 ※树专题-判断两个树是否相等

---
+++ src/leetcode/Solution100.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class Solution100 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int x) {
+            this.val = x;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+
+    }
+
+}
