7c59c57d7d1914c2e0fb60b103eb44b69dab2397
tenghaoxiang <873030913@qq.com>
2019-05-29 20:39:17 +0800

    2019.5.29 ※ 树专题-给n生成BST

--- src/leetcode/Solution95.java
+++ src/leetcode/Solution95.java
@@ -15,12 +15,39 @@ public class Solution95 {
         }
     }
 
+
+
+
     public List<TreeNode> generateTrees(int n) {
 
-        List<TreeNode> res = new ArrayList<>();
+        if (n == 0) {
+            List<TreeNode> list = new ArrayList<>();
+            return list;
+        }
+        return genTrees(1, n);
 
-        return res;
+    }
 
+    public List<TreeNode> genTrees(int start, int end) {
+        List<TreeNode> list = new ArrayList<>();
+        if (start > end) {
+            list.add(null);
+            return list;
+        }
+        List<TreeNode> left, right;
+        for (int i = start; i <= end; i++) {
+            left = genTrees(start, i - 1);
+            right = genTrees(i + 1, end);
+            for (TreeNode lnode : left) {
+                for (TreeNode rnode : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = lnode;
+                    root.right = rnode;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
     }
 
 }
