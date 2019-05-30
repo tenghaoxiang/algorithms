cdad5c7d8b259c70146c3321924322b48d29ba23
tenghaoxiang <873030913@qq.com>
2019-05-30 20:18:59 +0800

    2019.5.29 树专题-Z字型打印一颗树，链表也可以按索引插入

--- src/leetcode/Solution103.java
+++ src/leetcode/Solution103.java
@@ -1,4 +1,53 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Solution103 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int x) {
+            this.val = x;
+        }
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean zigzag = false;
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                if (zigzag) {
+                    level.add(0, node.val);
+                } else {
+                    level.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
+            zigzag = !zigzag;
+        }
+        return res;
+
+    }
+
 }
