77e5f287799b4a8e191ded412e81685c0078d0fe
tenghaoxiang <873030913@qq.com>
2019-06-02 22:34:52 +0800

    2019.6.2 ※ 树专题-判断一棵树高度是否平衡

---
+++ src/leetcode/Solution110.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+public class Solution110 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int x) {
+            this.val = x;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+
+        return help(root) != -1;
+
+    }
+
+    public int help(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = help(root.left);
+        int right = help(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+
+}
