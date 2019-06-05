083887127a3af65cbc53679e1032d5a6c6da67ea
tenghaoxiang <873030913@qq.com>
2019-06-05 20:53:51 +0800

    2019.6.5 先序遍历树

---
+++ src/leetcode/Solution144.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution144 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int x) {
+            this.val = x;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node != null) {
+                res.add(node.val);
+                stack.push(node.right);
+                stack.push(node.left);
+            }
+        }
+        return res;
+
+    }
+
+}
