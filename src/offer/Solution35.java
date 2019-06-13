69ad367d19d3abfe63d07a5289149144cebe18da
tenghaoxiang <873030913@qq.com>
2019-06-13 23:20:15 +0800

    2019.6.13 复制复杂链表，十分经典的解法！

---
+++ src/offer/Solution35.java
@@ -0,0 +1,50 @@
+package offer;
+
+public class Solution35 {
+
+    class RandomListNode {
+        int label;
+        RandomListNode next = null;
+        RandomListNode random = null;
+
+        RandomListNode(int label) {
+            this.label = label;
+        }
+    }
+
+    public RandomListNode Clone(RandomListNode pHead)
+    {
+
+        if (pHead == null) {
+            return null;
+        }
+        RandomListNode currentNode = pHead;
+
+        while (currentNode != null) {
+            RandomListNode cloneNode = new RandomListNode(currentNode.label);
+            RandomListNode nextNode = currentNode.next;
+            currentNode.next = cloneNode;
+            cloneNode.next = nextNode;
+            currentNode = nextNode;
+        }
+
+        currentNode = pHead;
+        while (currentNode != null) {
+            currentNode.next.random = currentNode.random == null ? null : currentNode.random.next;
+            currentNode = currentNode.next.next;
+        }
+
+        currentNode = pHead;
+        RandomListNode res = pHead.next;
+        while (currentNode != null) {
+            RandomListNode cloneNode = currentNode.next;
+            currentNode.next = cloneNode.next;
+            cloneNode.next = cloneNode.next == null ? null : cloneNode.next.next;
+            currentNode = currentNode.next;
+        }
+
+        return res;
+
+    }
+
+}
