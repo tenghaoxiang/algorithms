6ef2d49f022fe8ac7b2e05b5fc0da8d3d9400123
tenghaoxiang <873030913@qq.com>
2019-06-18 21:02:36 +0800

    2019.6.18 找出n个数中最小的K个数

---
+++ src/offer/Solution40.java
@@ -0,0 +1,14 @@
+package offer;
+
+import java.util.ArrayList;
+
+public class Solution40 {
+
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+
+    }
+
+}
