3e2d7ce48838fd8693f480f2c699128b77fbc26c
tenghaoxiang <873030913@qq.com>
2019-07-04 22:25:58 +0800

    2019.7.4 找出最小的k个数，用堆

--- src/offer/Solution40.java
+++ src/offer/Solution40.java
@@ -1,59 +1,84 @@
 package offer;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Solution40 {
 
+//    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+//
+//        ArrayList<Integer> list = new ArrayList<>();
+//        if (k <= 0 || k > input.length) {
+//            return list;
+//        }
+//        if (k == input.length) {
+//            for (int i = 0; i < k; i++) {
+//                list.add(input[i]);
+//            }
+//            return list;
+//        }
+//        int index = partition(input, 0, input.length - 1);
+//        while (index != k) {
+//            if (index > k) {
+//                index = partition(input, 0, index - 1);
+//            } else {
+//                index = partition(input, index + 1, input.length - 1);
+//            }
+//        }
+//        for (int i = 0; i < k; i++) {
+//            list.add(input[i]);
+//        }
+//        return list;
+//
+//    }
+//
+//    public int partition(int[] nums, int start, int end) {
+//        int num = nums[start];
+//        int left = start;
+//        int right = end + 1;
+//         while (true) {
+//            while (nums[++left] < num) {
+//                if (left >= end) {
+//                    break;
+//                }
+//            }
+//            while (nums[--right] > num) {
+//
+//            }
+//            if (left >= right) {
+//                break;
+//            }
+//            int m = nums[left];
+//            nums[left] = nums[right];
+//            nums[right] = m;
+//        }
+//        nums[start] = nums[right];
+//        nums[right] = num;
+//        return right;
+//    }
+
+    //最小堆方法
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
 
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> res = new ArrayList<>();
         if (k <= 0 || k > input.length) {
-            return list;
+            return res;
         }
-        if (k == input.length) {
-            for (int i = 0; i < k; i++) {
-                list.add(input[i]);
-            }
-            return list;
-        }
-        int index = partition(input, 0, input.length - 1);
-        while (index != k) {
-            if (index > k) {
-                index = partition(input, 0, index - 1);
-            } else {
-                index = partition(input, index + 1, input.length - 1);
+        PriorityQueue<Integer> heap = new PriorityQueue<>((num1, num2) -> num2.compareTo(num1));
+        for (int i = 0; i < input.length; i++) {
+            if (i < k) {
+                heap.offer(input[i]);
+            } else if (heap.peek() > input[i]) {
+                heap.poll();
+                heap.offer(input[i]);
             }
         }
         for (int i = 0; i < k; i++) {
-            list.add(input[i]);
+            res.add(heap.poll());
         }
-        return list;
+        return res;
 
-    }
-
-    public int partition(int[] nums, int start, int end) {
-        int num = nums[start];
-        int left = start;
-        int right = end + 1;
-         while (true) {
-            while (nums[++left] < num) {
-                if (left >= end) {
-                    break;
-                }
-            }
-            while (nums[--right] > num) {
-
-            }
-            if (left >= right) {
-                break;
-            }
-            int m = nums[left];
-            nums[left] = nums[right];
-            nums[right] = m;
-        }
-        nums[start] = nums[right];
-        nums[right] = num;
-        return right;
     }
 
 }
