c12b97bfa762283dcff3dffa82ad98b5d43827e7
tenghaoxiang <873030913@qq.com>
2019-07-07 22:50:32 +0800

    2019.7.5 把数组排成最小的数

---
+++ src/offer/Solution49.java
@@ -0,0 +1,25 @@
+package offer;
+
+public class Solution49 {
+
+    public int GetUglyNumber_Solution(int index) {
+        if (index < 7) {
+            return index;
+        }
+        int[] res = new int[index];
+        res[0] = 1;
+        int t2 = 0, t3 = 0, t5 = 0;
+        for (int i = 1; i < index; i++) {
+            res[i] = Math.min(Math.min(res[t2] * 2, res[t3] * 3), res[t5] * 5);
+            if (res[i] == res[t2] * 2) {
+                t2++;
+            } else if (res[i] == res[t3] * 3) {
+                t3++;
+            } else {
+                t5++;
+            }
+        }
+        return res[index - 1];
+    }
+
+}
