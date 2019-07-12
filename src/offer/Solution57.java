c7dbd3e13e56c4d777c4101b6355fe35ffddbb01
tenghaoxiang <873030913@qq.com>
2019-07-12 21:44:53 +0800

    2019.7.12 和为s的两个数字

--- src/offer/Solution57.java
+++ src/offer/Solution57.java
@@ -40,4 +40,38 @@ public class Solution57 {
 
     }
 
+    public ArrayList<Integer> FindNumbersWithSum(int [] array,int sum) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (array.length < 2) {
+            return res;
+        }
+        int cur;
+        int left = 0;
+        int right = array.length - 1;
+        int multi = Integer.MAX_VALUE;
+        while (right > left) {
+            cur = array[left] + array[right];
+            if (cur == sum) {
+                if (array[left] * array[right] < multi) {
+                    multi = array[left] * array[right];
+                    if (res.isEmpty()) {
+
+                    } else {
+                        res.remove(1);
+                        res.remove(0);
+                    }
+                    res.add(array[left]);
+                    res.add(array[right]);
+                }
+                left++;
+                right--;
+            } else if (cur < sum) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
+    }
+
 }
