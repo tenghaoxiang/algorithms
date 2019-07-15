db206aa6ed119541b28cda0fce49ee75b92d7fbd
tenghaoxiang <873030913@qq.com>
2019-07-15 20:38:10 +0800

    2019.7.15 反转字符串

--- src/offer/Solution58.java
+++ src/offer/Solution58.java
@@ -11,4 +11,33 @@ public class Solution58 {
 
     }
 
+    public String ReverseSentence(String str) {
+
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        char[] chars = str.toCharArray();
+        reverse(chars, 0, chars.length - 1);
+        int now = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == ' ') {
+                reverse(chars, now, i - 1);
+                now = i + 1;
+            }
+        }
+        reverse(chars, now, chars.length - 1);
+        return new String(chars);
+
+    }
+
+    public void reverse(char[] chars, int begin, int end) {
+        while (begin < end) {
+            char c = chars[begin];
+            chars[begin] = chars[end];
+            chars[end] = c;
+            begin++;
+            end--;
+        }
+    }
+
 }
