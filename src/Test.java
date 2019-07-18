bb8f4526820219af736dfe97556048051177d19b
tenghaoxiang <873030913@qq.com>
2019-07-18 22:45:27 +0800

    2019.7.18 HashMap get键为空很好的例子

---
+++ src/Test.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Test {
+
+    static volatile  HashMap<Integer,Integer> map = new HashMap<>();
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 13; i++) {
+                map.put(i,i);
+            }
+        });
+        Thread thread2 = new Thread(()->{
+            try {
+                Thread.sleep(1000*60);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(map.get(9));
+        });
+
+        thread2.start();
+        Thread.sleep(1000);
+        thread1.start();
+        Thread.sleep(1000*60);
+
+    }
+
+}
