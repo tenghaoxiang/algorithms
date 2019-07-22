011c32ced7dc52636db777f6463181a7c3000933
tenghaoxiang <873030913@qq.com>
2019-07-22 20:45:41 +0800

    2019.7.22 找出二叉树中下一个节点

---
+++ src/offer/Solution8.java
@@ -0,0 +1,38 @@
+package offer;
+
+public class Solution8 {
+
+    class TreeLinkNode {
+        int val;
+        TreeLinkNode left = null;
+        TreeLinkNode right = null;
+        TreeLinkNode next = null;
+
+        TreeLinkNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+
+        if (pNode == null) {
+            return null;
+        }
+        if (pNode.right != null) {
+            pNode = pNode.right;
+            while (pNode.left != null) {
+                pNode = pNode.left;
+            }
+            return pNode;
+        }
+        while (pNode.next != null) {
+            if (pNode.next.left == pNode) {
+                return pNode.next;
+            }
+            pNode = pNode.next;
+        }
+        return null;
+
+    }
+
+}
