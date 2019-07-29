339d006510a309bc4f0754c755425c4550bd99e8
tenghaoxiang <873030913@qq.com>
2019-07-29 23:07:39 +0800

    2019.7.29 祝自己生日快乐！- 找出数据流中的中位数

--- src/offer/Solution41.java
+++ src/offer/Solution41.java
@@ -1,5 +1,8 @@
 package offer;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * @author tenghaoxiang
  * @date 19-7-29 下午9:42
@@ -7,11 +10,36 @@ package offer;
  */
 public class Solution41 {
 
-    public void Insert(Integer num) {
+    int count;
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
 
+    public void Insert(Integer num) {
+        count++;
+        if ((count & 1) == 0) {
+            if (!maxHeap.isEmpty() && num < maxHeap.peek()) {
+                maxHeap.offer(num);
+                num = maxHeap.poll();
+            }
+            minHeap.offer(num);
+        } else {
+            if (!minHeap.isEmpty() && num > minHeap.peek()) {
+                minHeap.offer(num);
+                num = minHeap.poll();
+            }
+            maxHeap.offer(num);
+        }
     }
 
     public Double GetMedian() {
+
+        double result;
+        if ((count & 1) == 1) {
+            result = maxHeap.peek();
+        } else {
+            result = (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }
+        return result;
 
     }
 
