5cb09de7c08183d8f2e2d1d2699e9fff7d1cebeb
tenghaoxiang <873030913@qq.com>
2019-07-29 22:28:57 +0800

    2019.7.29 祝自己生日快乐！

---
+++ src/offer/Solution41.java
@@ -0,0 +1,18 @@
+package offer;
+
+/**
+ * @author tenghaoxiang
+ * @date 19-7-29 下午9:42
+ * @Description
+ */
+public class Solution41 {
+
+    public void Insert(Integer num) {
+
+    }
+
+    public Double GetMedian() {
+
+    }
+
+}
