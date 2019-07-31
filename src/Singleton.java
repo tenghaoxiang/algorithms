3c9dd24f5210059469aedc4fa06d16dd4ad5284a
tenghaoxiang <873030913@qq.com>
2019-07-31 19:01:50 +0800

    2019.7.31 三种单例模式

--- src/Singleton.java
+++ src/Singleton.java
@@ -5,12 +5,42 @@
  */
 public class Singleton {
 
-    static class SingletonHolder {
+    /**
+     * 基于类的初始化的解决方案
+     */
+    static class SingletonHolder1 {
         static Singleton instance = new Singleton();
     }
 
-    public static Singleton getInstance() {
-        return SingletonHolder.instance;
+    public static Singleton getInstance1() {
+        return SingletonHolder1.instance;
+    }
+
+    /**
+     * 基于双重锁检查的解决方案
+     */
+    private volatile static Singleton instance;
+
+    public static Singleton getInstance2() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                instance = new Singleton();
+            }
+        }
+        return instance;
+    }
+
+    /**
+     * 饿汉式
+     */
+    private static final Singleton INSTANCE = new Singleton();
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance3() {
+        return INSTANCE;
     }
 
 }
