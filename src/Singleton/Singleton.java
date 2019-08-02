dd6c058927c0dc2fc97ef7d422cbfba0ffd4c273
tenghaoxiang <873030913@qq.com>
2019-08-02 17:28:31 +0800

    2019.8.2 巩固单例模式

--- src/Singleton/Singleton.java
+++ src/Singleton/Singleton.java
@@ -7,12 +7,40 @@ package Singleton;
  */
 public class Singleton {
 
-    static class SingletonHolder {
-        static Singleton instance = new Singleton();
-    }
+    /**
+     * 基于类初始化
+     */
+//    static class SingletonHolder{
+//        static Singleton instance = new Singleton();
+//    }
+//
+//    public Singleton getInstance() {
+//        return SingletonHolder.instance;
+//    }
+
+    /**
+     * 基于双重锁检查
+     */
+//    private static volatile Singleton instance;
+//
+//    public static Singleton getInstance() {
+//        if (instance == null) {
+//            synchronized (Singleton.class) {
+//                if (instance == null) {
+//                    instance = new Singleton();
+//                }
+//            }
+//        }
+//        return instance;
+//    }
+
+    /**
+     * 饿汉式
+     */
+    private static final Singleton INSTANCE = new Singleton();
 
     public static Singleton getInstance() {
-        return SingletonHolder.instance;
+        return INSTANCE;
     }
 
 }
