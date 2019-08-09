2a8b754c9267a8493690228d4fd4e07c8c63ce59
tenghaoxiang <873030913@qq.com>
2019-08-09 19:05:00 +0800

    2019.8.9 二分查找

---
+++ src/important/BinarySearch.java
@@ -0,0 +1,29 @@
+package important;
+
+/**
+ * @author tenghaoxiang
+ * @date 2019/8/9 下午6:39
+ * @Description 二分查找
+ */
+public class BinarySearch <T extends Comparable> {
+
+    //数组必须是有序的
+    public int rank(T num, T[] array) {
+
+        int low = 0;
+        int high = array.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if (num.compareTo(array[mid]) < 0) {
+                high = mid - 1;
+            } else if (num.compareTo(array[mid]) > 0) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+
+}
