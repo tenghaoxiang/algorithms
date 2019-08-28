9efe71a8836d7e3939494a77560d5f251f449bd5
tenghaoxiang <873030913@qq.com>
2019-08-28 09:29:43 +0800

    2019.8.28 1、根据先序遍历和中序遍历构建二叉树 2、根据后序遍历和中序遍历构建二叉树

--- src/review/ConstructBinaryTree.java
+++ src/review/ConstructBinaryTree.java
@@ -1,12 +1,12 @@
-package important;
+package review;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author tenghaoxiang
- * @date 2019/8/27 下午4:41
- * @Description 1、根据先序遍历和中序遍历构建二叉树 2、根据后序遍历和中序遍历构建二叉树
+ * @date 2019/8/28 上午9:05
+ * @Description
  */
 public class ConstructBinaryTree {
 
@@ -55,15 +55,16 @@ public class ConstructBinaryTree {
     }
 
     public TreeNode buildTree2(int[] inorder, int startIn, int endIn, int[] postorder, int startPost, int endPost, Map<Integer, Integer> map) {
+
         if (startIn > endIn || startPost > endPost) {
             return null;
         }
-        TreeNode node = new TreeNode(postorder[endPost]);
-        int i = map.get(node.val);
-        node.left = buildTree2(inorder, startIn, i - 1, postorder, startPost, startPost + i - 1 - startIn, map);
-        node.right = buildTree2(inorder, i + 1, endIn, postorder, startPost + i - startIn, endPost - 1, map);
-        return node;
-    }
+        TreeNode root = new TreeNode(postorder[endPost]);
+        int i = map.get(root.val);
+        root.left = buildTree2(inorder, startIn, i - 1, postorder, startPost, startPost + i - startIn - 1, map);
+        root.right = buildTree2(inorder, i + 1, endIn, postorder, startPost + i - startIn, endPost - 1, map);
+        return root;
 
+    }
 
 }
