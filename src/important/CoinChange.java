3752c08854625fa54ca85b3aa7e1927b6e543d10
tenghaoxiang <873030913@qq.com>
2019-09-06 21:48:58 +0800

    2019.9.6 整钱换硬币，最少硬币数

---
+++ src/important/CoinChange.java
@@ -0,0 +1,33 @@
+package important;
+
+/**
+ * @author tenghaoxiang
+ * @date 2019/9/6 下午9:24
+ * @Description
+ */
+public class CoinChange {
+
+    public int coinChange(int[] coins, int amount) {
+
+        if (amount < 0) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        int sum = 0;
+        while (++sum <= amount) {
+            int min = -1;
+            for (int coin : coins) {
+                if (sum >= coin && dp[sum - coin] != -1) {
+                    int temp = dp[sum - coin] + 1;
+                    if (min < 0 || temp < min) {
+                        min = temp;
+                    }
+                }
+            }
+            dp[sum] = min;
+        }
+        return dp[amount];
+
+    }
+
+}
